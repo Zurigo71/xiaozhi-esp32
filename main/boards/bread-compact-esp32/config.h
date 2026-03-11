751c0c364ef03bb8d41ec0b1e4f14f4b5d437e68
Zurigo71 <massimopandolfini7@gmail.com>
2026-03-11 12:26:31 +0100

    fix batteria

--- main/boards/bread-compact-esp32/config.h
+++ main/boards/bread-compact-esp32/config.h
@@ -6,53 +6,38 @@
 #define AUDIO_INPUT_SAMPLE_RATE  16000
 #define AUDIO_OUTPUT_SAMPLE_RATE 24000
 
-// 如果使用 Duplex I2S 模式，请注释下面一行
 #define AUDIO_I2S_METHOD_SIMPLEX
 
 #ifdef AUDIO_I2S_METHOD_SIMPLEX
+// Configurazione Microfono (4, 5, 6)
+#define AUDIO_I2S_MIC_GPIO_WS   GPIO_NUM_4
+#define AUDIO_I2S_MIC_GPIO_SCK  GPIO_NUM_5
+#define AUDIO_I2S_MIC_GPIO_DIN  GPIO_NUM_6
 
-#define AUDIO_I2S_MIC_GPIO_WS   GPIO_NUM_25
-#define AUDIO_I2S_MIC_GPIO_SCK  GPIO_NUM_26
-#define AUDIO_I2S_MIC_GPIO_DIN  GPIO_NUM_32
-
-#define AUDIO_I2S_SPK_GPIO_DOUT GPIO_NUM_33
-#define AUDIO_I2S_SPK_GPIO_BCLK GPIO_NUM_14
-#define AUDIO_I2S_SPK_GPIO_LRCK GPIO_NUM_27
-
-#else
-
-#define AUDIO_I2S_GPIO_WS GPIO_NUM_4
-#define AUDIO_I2S_GPIO_BCLK GPIO_NUM_5
-#define AUDIO_I2S_GPIO_DIN  GPIO_NUM_6
-#define AUDIO_I2S_GPIO_DOUT GPIO_NUM_7
-
+// Configurazione Amplificatore (7, 15, 16)
+#define AUDIO_I2S_SPK_GPIO_DOUT GPIO_NUM_7
+#define AUDIO_I2S_SPK_GPIO_BCLK GPIO_NUM_15
+#define AUDIO_I2S_SPK_GPIO_LRCK GPIO_NUM_16
 #endif
 
-#define BOOT_BUTTON_GPIO        GPIO_NUM_0
-#define TOUCH_BUTTON_GPIO       GPIO_NUM_5
-#define ASR_BUTTON_GPIO         GPIO_NUM_19
-#define BUILTIN_LED_GPIO        GPIO_NUM_2
+#define BOOT_BUTTON_GPIO         GPIO_NUM_0
+#define BUILTIN_LED_GPIO         GPIO_NUM_2
 
-#define ML307_RX_PIN            GPIO_NUM_16
-#define ML307_TX_PIN            GPIO_NUM_17
-
-#define DISPLAY_SDA_PIN GPIO_NUM_4
-#define DISPLAY_SCL_PIN GPIO_NUM_15
-#define DISPLAY_WIDTH   128
-
-#if CONFIG_OLED_SSD1306_128X32
-#define DISPLAY_HEIGHT  32
-#elif CONFIG_OLED_SSD1306_128X64
-#define DISPLAY_HEIGHT  64
-#else
-#error "OLED display type is not selected"
-#endif
+// Pin del Display (Spostati per non andare in conflitto con l'audio)
+#define DISPLAY_SDA_PIN          GPIO_NUM_41 
+#define DISPLAY_SCL_PIN          GPIO_NUM_42
+#define DISPLAY_WIDTH            128
+#define DISPLAY_HEIGHT           64
 
 #define DISPLAY_MIRROR_X true
 #define DISPLAY_MIRROR_Y true
 
+// --- CONFIGURAZIONE BATTERIA PIN 14 ---
+#define CONFIG_BAT_ADC_CHAN     ADC_CHANNEL_3
+#define CONFIG_BAT_ADC_UNIT     ADC_UNIT_1
+#define CONFIG_BAT_UPPER_RES    100.0
+#define CONFIG_BAT_LOWER_RES    100.0
 
-// A MCP Test: Control a lamp
-#define LAMP_GPIO GPIO_NUM_18
+#define LAMP_GPIO               GPIO_NUM_NC
 
 #endif // _BOARD_CONFIG_H_
