9448daa7dc5e927c9583eb8006a88cb84fa31d44
Zurigo71 <massimopandolfini7@gmail.com>
2026-03-11 14:44:00 +0100

    test finale batteria
    Removed unused pin definitions for audio and display.

--- main/boards/bread-compact-esp32/config.h
+++ main/boards/bread-compact-esp32/config.h
@@ -3,19 +3,14 @@
 
 #include <driver/gpio.h>
 
-// Audio I2S
 #define AUDIO_I2S_BCK_PIN      (GPIO_NUM_7)
 #define AUDIO_I2S_WS_PIN       (GPIO_NUM_15)
 #define AUDIO_I2S_DATA_OUT_PIN (GPIO_NUM_16)
 #define AUDIO_I2S_DATA_IN_PIN  (GPIO_NUM_38)
 
-// Display ST7789 (SPI)
 #define LCD_SDA_PIN            (GPIO_NUM_10)
 #define LCD_SCL_PIN            (GPIO_NUM_9)
-#define LCD_DC_PIN             (GPIO_NUM_46)
-#define LCD_RES_PIN            (GPIO_NUM_11)
 
-// Batteria
 #define BATTERY_ADC_PIN        (GPIO_NUM_14)
 
 #endif
